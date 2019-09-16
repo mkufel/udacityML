d39c72e32e62fde2edf7bfcc4efe1416bc7e4b15
mkufel <m.kufel@hotmail.com>
2019-09-16 23:34:30 +0200

    SVM mini-script

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -23,8 +23,30 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
 
-#########################################################
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+import numpy as np
 
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
+t0 = time()
+clf = svm.SVC(kernel='rbf', C=10000.0)
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "training time:", round(time()-t1, 3), "s"
+
+print accuracy_score(pred, labels_test)
+
+print "Accuracy 10th elt: " + str(pred[10])
+print "Accuracy 26th elt: " + str(pred[26])
+print "Accuracy 50th elt: " + str(pred[50])
+
+unique, counts = np.unique(pred, return_counts=True)
+print dict(zip(unique, counts))
+
+print "Number of test samples classified as 1: " + str(sum(pred == 1))

927389e59dbcaaed2eb26f18feb4b952178c895d
mkufel <m.kufel@hotmail.com>
2019-10-17 19:20:16 +0200

    outliers cleaning

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -13,8 +13,11 @@ def outlierCleaner(predictions, ages, net_worths):
     
     cleaned_data = []
 
-    ### your code goes here
+    errors = [abs(pred - net_worth) for pred, net_worth in zip(predictions, net_worths)]
+    data = zip(ages, net_worths, errors)
+    data.sort(key=lambda tup: tup[2])  # sort by error
+    new_length = int(len(predictions) * 0.9)  # set the new length without the last 10%
+    cleaned_data = data[:new_length]   # trim the array
 
-    
     return cleaned_data
 
